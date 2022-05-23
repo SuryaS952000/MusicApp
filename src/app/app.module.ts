aa4f49ea0b6d4b64ba1e60034a978ab92fcdbdbd
Surya S <palashsurya95@gmail.com>
2022-05-23 18:29:07 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AllsongsComponent } from './allsongs/allsongs.component';
+import { SongComponent } from './song/song.component';
+import { FavComponent } from './fav/fav.component';
+import { GenreComponent } from './genre/genre.component';
+//import { AngularTypewriterEffectModule } from 'angular-typewriter-effect';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    AllsongsComponent,
+    SongComponent,
+    FavComponent,
+    GenreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //AngularTypewriterEffectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
